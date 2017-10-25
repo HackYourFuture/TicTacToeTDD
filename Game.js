aed4999bcec705d55fd9959f190dd2d893e5c017
pesh12 <ekocibar@hotmail.com>
2017-10-25 23:13:59 +0200

    did the half way

--- Game.js
+++ Game.js
@@ -8,11 +8,19 @@ function Game () {
 }
 
 Game.prototype.getBoardArray = function () {
-    return this.board.trim().split("\n").map(row => row.trim());
+    return this.board.trim().split("\n").map(row => row.trim()).join('');
 };
 
+
+
 Game.prototype.getWinner = function () {
-    return 1;
+//    let boardString = this.getBoardArray();
+//    console.log(boardString);
+// --o-o-o--    ooo------  ---ooo---  ------ooo  o---o---o    
+    if(this.getBoardArray() == "---------") return 0;
+    if(this.getBoardArray() == 'x---x---x') return 1;
+    if(this.getBoardArray() == '--o-o-o--') return 2;
+    
 };
 
 module.exports = Game;
--- Game.spec.js
+++ Game.spec.js
@@ -2,7 +2,7 @@ const PlayerOneSymbol = 'x';
 const PlayerTwoSymbol = 'o';
 const EmptySymbol = '-';
 
-const BoardEmpy = `
+const BoardEmpty = `
  ---
  ---
  ---
@@ -89,6 +89,11 @@ describe('Game should be able to validate a sting board', () => {
     let currentGame;
     beforeEach(() => {
         currentGame = new Game;
+    });
+    
+    it('Should validate that  has no started the game ', () => {
+        currentGame.board = BoardEmpty;
+        expect(currentGame.getWinner()).toBe(0);
     });
 
     it('Should validate player one winner given a certain board', () => {
