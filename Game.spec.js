0cb1b562a8e47df043eda50ce861f2cc707d83a4
Mahmoud Hasan <30556626+itemah26@users.noreply.github.com>
2017-10-25 19:08:30 +0200

    Update Game.spec.js

--- Game.spec.js
+++ Game.spec.js
@@ -2,17 +2,38 @@ const PlayerOneSymbol = 'x';
 const PlayerTwoSymbol = 'o';
 const EmptySymbol = '-';
 
-const BoardEmpy = `
- ---
- ---
- ---
+const EmptyBoard = `
+---
+---
+---
 `;
-
-const BoardPlayerOneWins = `
- x--
- -x-
- --x
-`;
+const BoardPlayerOneWins = [
+    `
+        --x
+        -x-
+        x--
+    `,
+    `
+       xxx
+       ---
+       ---
+    `,
+    `
+       ---
+       xxx
+       ---
+    `,
+    `
+       ---
+       ---
+       xxx
+    `,
+    `
+       x--
+       -x-
+       --x
+    `
+];
 
 const BoardPlayerTwoWins = [
     `
@@ -42,63 +63,63 @@ const BoardPlayerTwoWins = [
     `
 ];
 
-const Game = require('./Game');
-
+const game = require('./game');
 
 describe("Tic Tac Toe Game", () => {
 
-    let currentGame;
-    beforeEach(() => {
-        currentGame = new Game;
-    });
+     let currentGame;
+     beforeEach(() => {
+          currentGame = new game;
+     });
 
-    it('Should create a game', () => {
+     it('Should create a game', () => {
+          expect(currentGame instanceof game).toBe(true);
+     });
 
-        expect(currentGame instanceof Game).toBe(true);
+     it('Should have a board', () => {
+          let currentGame = new game;
+          expect(currentGame.board).toBeDefined();
 
-    });
+     });
 
-    it('Should have a board', () => {
+     it('Should have one player', () => {
+          expect(currentGame.playerOne).toBeDefined();
+     });
 
-        expect(currentGame.board).toBeDefined();
-
-    });
-
-    it('Should have one player', () => {
-
-        expect(currentGame.playerOne).toBeDefined();
-
-    });
-
-    it('Should have two players', () => {
-
-        expect(currentGame.playerTwo).toBeDefined();
-
-    });
-
-    it('Should have a function to determinate winner', () => {
-
-        expect(currentGame.getWinner).toBeDefined();
-
-    });
+     it('Should have two player', () => {
+          expect(currentGame.playerTwo).toBeDefined();
+     });
+     it('Should have a function to determinate a winner', () => {
+          expect(currentGame.getWinner).toBeDefined();
+     });
 
 });
 
-describe('Game should be able to validate a sting board', () => {
+describe('Game should be able to validate a string board', () => {
+     let currentGame;
+     beforeEach(() => {
+          currentGame = new game;
 
-    let currentGame;
-    beforeEach(() => {
-        currentGame = new Game;
-    });
 
-    it('Should validate player one winner given a certain board', () => {
-        currentGame.board = BoardPlayerOneWins;
-        expect(currentGame.getWinner()).toBe(1);
-    });
+     });
+     it('Should validate player one wiwnner given a certain board', () => {
+          currentGame.board = BoardPlayerOneWins;
+          expect(currentGame.getWinner()).toBe(1);
+     });
+     it('Should validate player two wiwnner given a certain board', () => {
+          currentGame.board = BoardPlayerTwoWins;
+          expect(currentGame.getWinner()).toBe(2);
+     });
 
-    it('Should validate player two winner given a certain board', () => {
-        currentGame.board = BoardPlayerTwoWins;
-        expect(currentGame.getWinner()).toBe(2);
-    });
 
 });
+describe('Game board is empty', () => {
+     let currentGame;
+     beforeEach(() => {
+          currentGame = new game;
+     });
+     it('Should have empty board', () => {
+          currentGame.board = EmptyBoard;
+          expect(currentGame.getGameBoard()).toBe(null);
+     });
+})
