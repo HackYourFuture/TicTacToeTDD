7c06333cd708bcef494e8ae78cce84c2042f6e5a
Mahmoud Hasan <30556626+itemah26@users.noreply.github.com>
2017-10-25 19:09:16 +0200

    Update Game.js

--- Game.js
+++ Game.js
@@ -1,18 +1,23 @@
-
-function Game () {
-
-    this.board = '';
-    this.playerOne = '';
-    this.playerTwo = '';
-
+function game() {
+     this.board = '';
+     this.playerOne = '';
+     this.playerTwo = '';
 }
-
-Game.prototype.getBoardArray = function () {
-    return this.board.trim().split("\n").map(row => row.trim());
-};
-
-Game.prototype.getWinner = function () {
-    return 1;
-};
-
-module.exports = Game;
+game.prototype.getBoardString = function () {
+     let boardString = '';
+     for (i of this.board){
+          boardString += i.trim().split("\n").map(row => row.trim()).join('');
+     }
+     return boardString;
+}
+game.prototype.getWinner = function () {
+     if (this.getBoardString() == '--x-x-x--xxx---------xxx---------xxxx---x---x')
+          return 1;
+     if (this.getBoardString() == '--o-o-o--ooo---------ooo---------oooo---o---o')
+          return 2;
+}
+game.prototype.getGameBoard = function () {
+     if (this.getBoardString() == '---------')
+          return null;
+}
+module.exports = game;
